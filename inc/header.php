4472a5e07b0f5eebbe16f68067a340640954e62a
Jeff Tench <jeff.m.tench@gmail.com>
2013-07-26 14:33:13 -0700

    Dogwood Naturals code

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Dogwood Naturals</title>
+	<link href='http://fonts.googleapis.com/css?family=Arbutus+Slab' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+</head>
+<body>
+	<div class="header">
+		<h1>Dogwood Naturals</h1>
+		<p id="nav">
+			<a href="index.php">Home</a> 
+			<a href="catalog.php">Catalog</a>
+			<a href="blog.php">Blog</a>
+			<a href="about_us.php">About Us</a>
+		</p>
+	</div>
---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>Dogwood Naturals</title>
+	<link href='http://fonts.googleapis.com/css?family=Arbutus+Slab' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+</head>
+<body>
+	<div class="header">
+		<h1>Dogwood Naturals</h1>
+		<p>
+			<a href="index.php" class="nav">Home</a> 
+			<a href="catalog.php"  class="nav">Catalog</a>
+			<a href="blog.php"  class="nav">Blog</a>
+			<a href="about_us.php"  class="nav">About Us</a>
+		</p>
+	</div>
+	<div class="products">
+
+		<p>
+			<img src="img/cmc_lanloaf.jpg" alt="Crow Mountain Crafts Lanolin Loaf">
+			<img src="img/cmc_lanmist.jpg" alt="Crow Mountain Crafts Lanolin Mist">
+			<img src="img/cmc_lipb.jpg" alt="Crow Mountain Crafts Lip Balm">
+			<img src="img/cmc_lww.jpg" alt="Crow Mountain Crafts Lanolin Wool Wash">
+			<img src="img/cmc_soap.jpg" alt="Crow Mountain Crafts Soap">
+
+		</p>
+
+	</div>
+
+</body>
+
+</html>
